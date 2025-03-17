15c98519cedc60400b127a37567e8c367cd51958
liam-621 <195730031+liam-621@users.noreply.github.com>
2025-03-17 11:23:15 +1100

    Finish change squares functionality

--- script.js
+++ script.js
@@ -3,18 +3,29 @@ const container = document.querySelector("#container");
 for (i = 0; i < 256; i++) {
     const div = document.createElement("div");
     div.setAttribute("id", "grid");
+    div.style.width = "55px";
     container.appendChild(div);
 }
 
 container.addEventListener("mouseover", (e) => {
     let target = e.target;
-    target.style.background = "blue";
-
+    if (target.id === "grid") {
+        target.style.background = "blue";
+    }
 })
 
 const btn = document.querySelector("button");
 
 btn.addEventListener("click", () => {
     let squares = prompt("Enter number of squares per side for new grid");
+    container.innerHTML = ""; // Clear the grid
+    for (i = 0; i < squares * squares; i++) {
+        const div = document.createElement("div");
+        div.setAttribute("id", "grid");
+        div.style.width = `${880 / squares}px`;
+        container.appendChild(div);
+    }
+    
 })
+
 
